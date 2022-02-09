b3b539e7dfd83a90154b9a092c90747d14335d0c
Dexter <dexter.edwards93@gmail.com>
2022-02-09 09:16:15 +0000

    try generating hash

--- api/main.go
+++ api/main.go
@@ -4,6 +4,8 @@ import (
 	// "bytes"
 	//  "compress/gzip"
 
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	_ "encoding/json"
 	"errors"
@@ -32,6 +34,7 @@ type SignedTx struct {
 	Sig     string
 	PubKey  string
 	Nonce   uint64
+	TxHash  string
 }
 
 func getCommand(inputData *commandspb.InputData) (protoiface.MessageV1, string, error) {
@@ -90,6 +93,10 @@ func unpackSignedTx(rawtx []byte) (interface{}, error) {
 		return nil, err
 	}
 
+	hasher := sha256.New()
+	hasher.Write(rawtx)
+    hash := hex.EncodeToString(hasher.Sum(nil))
+	
 	inputData := commandspb.InputData{}
 	err = proto.Unmarshal(tx.InputData, &inputData)
 	if err != nil {
@@ -113,6 +120,7 @@ func unpackSignedTx(rawtx []byte) (interface{}, error) {
 		Sig:     tx.Signature.Value,
 		PubKey:  "0x" + tx.GetPubKey(),
 		Nonce:   inputData.Nonce,
+		TxHash:  hash,
 	}, nil
 }
 
